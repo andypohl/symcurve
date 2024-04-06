b4870cec427b682bbe0f10f5a7360317530175c2
Andy Pohl <andy@pohl.io>
2024-04-05 22:43:43 -0500

    fluff

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,8 @@ use clap::Parser;
 
 fn main() {
     let cli = Cli::parse();
-
-    // You can check the value provided by positional arguments, or option arguments
     let input = cli.input.to_str().unwrap();
     if !input.is_empty() {
         println!("Value for input: {input}");
     }
-
-    // Continued program logic goes here...
 }
