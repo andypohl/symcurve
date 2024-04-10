46afabbed1cae9cdd0419e00d821967dc31e6bd0
Andy Pohl <andy@pohl.io>
2024-04-10 00:23:33 -0500

    RecordPiece struct

--- src/fasta.rs
+++ src/fasta.rs
@@ -1,8 +1,34 @@
 //! Functions for working with FASTA files.
 
+use std::rc::Rc;
+
 use noodles_core::Position;
-use noodles_fasta::record::Definition;
+use noodles_fasta::record::Sequence;
 use noodles_fasta::{self, Record};
+
+/// One Record will be split into multiple RecordPieces.
+/// The original Record is kept as an Rc so that each of the
+/// RecordPieces can share the same ownership.
+pub struct RecordPiece {
+    pub record: Rc<Record>,
+    pub start: Position,
+    pub end: Position,
+}
+
+impl RecordPiece {
+    fn new(record: Rc<Record>, start: Position, end: Position) -> Self {
+        Self {
+            record,
+            start,
+            end,
+        }
+    }
+
+    /// Get the sequence of the RecordPiece by slicing into the original Record.
+    pub fn sequence(&self) -> Sequence {
+        self.record.sequence().slice(self.start..=self.end).unwrap()
+    }
+}
 
 #[allow(dead_code)]
 /// Given a record, split the sequence by runs of Ns.
@@ -26,7 +52,7 @@ use noodles_fasta::{self, Record};
 /// >chr42 13-17
 /// ATGCA
 /// ```
-pub fn split_seq_by_n(record: Record) -> Vec<Record> {
+pub fn split_seq_by_n(record: Record) -> Vec<RecordPiece> {
     let mut records = Vec::new();
     let n = record.sequence().len();
     let seq = record.sequence().as_ref();
@@ -46,11 +72,9 @@ pub fn split_seq_by_n(record: Record) -> Vec<Record> {
             // Position is 1-based so add 1 to left
             let start = Position::try_from(left + 1).unwrap();
             let end = Position::try_from(right).unwrap();
-            let subseq = record.sequence().slice(start..=end).unwrap();
-            let info = format!("{}-{}", start, end).as_bytes().to_vec();
-            let def = Definition::new(record.name(), Some(info));
-            let rec = Record::new(def, subseq);
-            records.push(rec);
+            let rec_rc = Rc::new(record.to_owned());
+            let piece = RecordPiece::new(rec_rc, start, end);
+            records.push(piece);
         }
     }
     records
@@ -95,18 +119,10 @@ mod tests {
             .flat_map(|rec| split_seq_by_n(rec.unwrap()))
             .collect();
         assert_eq!(split_records.len(), 2);
-        //println!("{:?}", split_records[1]);
         assert_eq!(split_records[0].sequence().as_ref(), b"ATGCATGC".to_vec());
         assert_eq!(split_records[1].sequence().as_ref(), b"ATGCA".to_vec());
-        // check the descriptions where the ranges are stashed
-        assert_eq!(
-            split_records[0].definition().description().unwrap(),
-            b"1-8".to_vec()
-        );
-        assert_eq!(
-            split_records[1].definition().description().unwrap(),
-            b"13-17".to_vec()
-        );
+        assert_eq!(usize::from(split_records[1].start), 13);
+        assert_eq!(usize::from(split_records[1].end), 17);
     }
 
     #[test]
